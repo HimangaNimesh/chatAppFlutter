d3601fe9a070a0dbc3bf7a3412839ea1e56b768e
HimangaNimesh <himanganimesh2@gmail.com>
2022-09-29 23:42:12 +0530

    Login completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:chat_app/helper/helper_function.dart';
+import 'package:chat_app/pages/auth/login_page.dart';
+import 'package:chat_app/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -7,13 +10,36 @@ void main() async{
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus()async{
+    await HelperFunctions.getUserLoggedInStatus().then((value) {
+      if(value!=null){
+        _isSignedIn = value;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const Homepage() : const LoginPage(),
     );
   }
 }
